ca2430ff35d0c8636c8a88b2e7edca834084e418
Koji AGAWA <atty303@gmail.com>
2018-09-10 00:21:40 +0900

    wip

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # vo:Marionette
 
+## Prerequiesties
+
+* CMake 3.12
+
+## How to build
+
+
+
+```
+
+cmake -H. -Bbuild -G "Visual Studio 15 2017"
---
+++ injector/controller/Main.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using CommandLine;
+
+namespace VoMarionette {
+  public class Options
+  {
+    [Option('e', "executable", Default = "C:\\Program Files (x86)\\AHS\\VOICEROID+\\ZunkoEX\\VOICEROID.exe")]
+    public string ExecutableFileName { get; set; }
+
+    public string ProfileDllFileName
+    {
+      get
+      {
+        var pwd = System.IO.Directory.GetCurrentDirectory();
+        return Path.Combine(pwd, "Profiler.dll");
+      }
+    }
+  }
+
+  public class Controller {
+    const string PROFILER_UUID = "{75C1EAAA-9D03-48DB-8209-BF45282EB0BC}";
+
+    public static void Main(string[] args) {
+      //System.Diagnostics.Debug.WriteLine("hogehoge");
+      CommandLine.Parser.Default.ParseArguments<Options>(args)
+        .WithParsed(o => new Controller().Run(o));
+    }
+
+    private int Run(Options opts)
+    {
+      var psi = new ProcessStartInfo
+      {
+        FileName = opts.ExecutableFileName,
+        UseShellExecute = false
+      };
+
+      
+      SetupProfiler(psi.EnvironmentVariables, PROFILER_UUID, opts.ProfileDllFileName);
+
+      using (var process = new System.Diagnostics.Process())
+      {
+        process.StartInfo = psi;
+        process.Start();
+        process.WaitForExit();
+      }
+
+      return 0;
+    }
+
+    void SetupProfiler(StringDictionary envs, string uuid, string path)
+    {
+      // Settings Up a Profiling Environment
+      // https://docs.microsoft.com/ja-jp/dotnet/framework/unmanaged-api/profiling/setting-up-a-profiling-environment
+      envs.Add("COR_ENABLE_PROFILING", "1");
+      envs.Add("COR_PROFILER", uuid);
+
+      // Registry-Free Profiler Startup and Attach
+      // https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-4.0/ee471451(v%3dvs.100)
+      envs.Add("COR_PROFILER_PATH", path);
+
+      // Force .NET 4.0
+      envs.Add("COMPLUS_Version", "v4.0.30319");
+
+      // Profiler Compatibility Settings
+      // https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-4.0/dd778910(v%3dvs.100)
+      envs.Add("COMPLUS_ProfAPI_ProfilerCompatibilitySetting", "EnableV2Profiler");
+    }
+  }
+}
